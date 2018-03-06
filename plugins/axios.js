f7f974f5b297c9cd1876e7f6c5f11fe94b5929ef
Trys <trysmudford@gmail.com>
2018-03-06 22:15:25 +0000

    Logging

--- plugins/axios.js
+++ plugins/axios.js
@@ -23,6 +23,7 @@ if (process.server && process.static) {
   instance.interceptors.response.use(
     async function (response) {
       // Do something with response data
+      console.log(response.request.path)
       const end = response.request.path.includes('.netlify/functions') ? '' : '.json'
       const path = join(process.env.dataDir, response.request.path + end)
       console.log('Save', path)
