97e443ef2abc5c136a0e2b83f496b8591783dc4d
Trys <trysmudford@gmail.com>
2018-06-11 19:52:24 +0100

    Confirm password

--- pages/account/signup.vue
+++ pages/account/signup.vue
@@ -12,6 +12,7 @@
       <label>Username: <br><input class="input-text" type="text" autocomplete="off" name="username" required></label><br>
       <label>Email: <br><input type="email" name="email" autocomplete="email" required></label><br>
       <label>Password: <br><input type="password" name="password" autocomplete="off" required></label><br>
+      <label>Confirm Password: <br><input type="password" name="confirm_password" autocomplete="off" required></label><br>
       <button class="button" type="submit">Sign up</button>
     </form>
   </center-page>  
@@ -41,9 +42,19 @@ export default {
   methods: {
     signup (event) {
       this.error = ''
+      
+      const pw = event.target.password
+      const cpw = event.target.confirm_password
+
+      if (!pw.value || !cpw.value || pw.value !== cpw.value) {
+        this.error = 'Check both passwords match'
+        cpw.value = ''
+        return
+      }
+
       this.$store.state.auth.signup(
         event.target.email.value,
-        event.target.password.value,
+        pw.value,
         { full_name: event.target.username.value }
       ).then(
         response => {
@@ -55,7 +66,8 @@ export default {
             event.target.username.value = ''
             this.error = 'Someone with that username already exists'
           }
-          event.target.password.value = ''
+          pw.value = ''
+          cpw.value = ''
         }
       )
     },
